de260ac153f2da520600019873a889ac9f0b2c9d
DigitalJersey-Git <apister@pint.com>
2017-04-03 18:15:03 +0100

    Added stuff

---
+++ JerseyWweather/JerseyWweather/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  JerseyWweather
+//
+//  Created by Pister Family on 3/30/17.
+//  Copyright © 2017 MulgaKittyProductions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var JerseyClick: UIButton!
+    @IBOutlet weak var GuernseyClick: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
