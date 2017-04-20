13383d50e3954c09919742442557ea3e21bf5fd5
DigitalJersey-Git <apister@pint.com>
2017-04-20 15:42:56 +0100

    adding what has changed

--- JerseyWweather/JerseyWweather/ViewController.swift
+++ JerseyWweather/JerseyWweather/ViewController.swift
@@ -8,12 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class station: NSObject {
+    var wind: String?
+    var Gust: String?
+    var Visibility: String?
+    var Weather: String?
+    var Pressure: Int?
+    var Tendency: String?
+    
+    override init() {
+        super.init()
+        
+        
+    }
+}
+
+class ViewController: UIViewController, XMLParserDelegate  {
     
     @IBOutlet weak var JerseyClick: UIButton!
     @IBOutlet weak var GuernseyClick: UIButton!
     
     var selectedIsland: String?
+    var xmlData: String?
+    var weatherReport = [String: station]()
     
     @IBAction func GuernesyBPush(_ sender: Any) {
         let Btn = sender as! UIButton
@@ -35,9 +52,43 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        if let path = Bundle.main.path(forResource: "jerseymet", ofType: "xml") {
+            let fm = FileManager()
+            let exists = fm.fileExists(atPath: path)
+            if (exists) {
+                if let c = fm.contents(atPath: path) {
+                    xmlData = String(bytes: c, encoding: String.Encoding.utf8)
+                    if let xdata = xmlData?.data(using: String.Encoding.utf8) {
+                        let parser = XMLParser(data: xdata)
+                        parser.delegate = self
+                        print("Result: \(parser.parse())")
+                    }
+                }
+            }
+        }
         
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        
+        if (elementName as String) == "StationReport" {
+            let newstation = station()
+            if let key = attributeDict["Name"] {
+                weatherReport.updateValue(newstation, forKey: key)
+            }
+        }
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
