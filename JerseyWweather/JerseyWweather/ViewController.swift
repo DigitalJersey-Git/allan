ce0d9e8b4320421a48816d6d824c000f52c7e651
Allan Pister <allan@muglakitty.com>
2017-04-20 17:41:41 +0100

    adding new updates

--- JerseyWweather/JerseyWweather/ViewController.swift
+++ JerseyWweather/JerseyWweather/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class station: NSObject {
+class weatherStation: NSObject {
     var wind: String?
     var Gust: String?
     var Visibility: String?
@@ -25,20 +25,22 @@ class station: NSObject {
 
 class ViewController: UIViewController, XMLParserDelegate  {
     
-    @IBOutlet weak var JerseyClick: UIButton!
-    @IBOutlet weak var GuernseyClick: UIButton!
+    @IBOutlet weak var stationList: UIButton!
+    @IBOutlet weak var mylocation: UIButton!
     
     var selectedIsland: String?
     var xmlData: String?
-    var weatherReport = [String: station]()
+    var weatherReports = [String: weatherStation]()
+    var currentStation: weatherStation?
+    var foundCharacters: String = String()
     
-    @IBAction func GuernesyBPush(_ sender: Any) {
+    @IBAction func weatherLocBPush(_ sender: Any) {
         let Btn = sender as! UIButton
         self.setSelectedIs(sender: Btn)
-        self.performSegue(withIdentifier: "islandweatherSegue", sender: self)
+        self.performSegue(withIdentifier: "stationListingSegue", sender: self)
     }
     
-    @IBAction func jerseyBPush(_ sender: Any) {
+    @IBAction func myLocationBPush(_ sender: Any) {
         let Btn = sender as! UIButton
         self.setSelectedIs(sender: Btn)
         self.performSegue(withIdentifier: "islandweatherSegue", sender: self)
@@ -74,21 +76,47 @@ class ViewController: UIViewController, XMLParserDelegate  {
     func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
         
         if (elementName as String) == "StationReport" {
-            let newstation = station()
-            if let key = attributeDict["Name"] {
-                weatherReport.updateValue(newstation, forKey: key)
+            self.currentStation = weatherStation()
+            if let key = attributeDict["Name"], self.currentStation != nil {
+                weatherReports.updateValue(self.currentStation!, forKey: key)
             }
         }
     }
     
     func parser(_ parser: XMLParser, foundCharacters string: String) {
         
+        if self.currentStation != nil {
+            let data = string.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
+            
+            if (!data.isEmpty) {
+                self.foundCharacters += data
+            }
+        }
     }
     
     func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
         
+        print(elementName)
+        let lowerText = elementName.lowercased()
+        switch(lowerText) {
+            case "wind":
+                self.currentStation?.wind = self.foundCharacters
+            case "gust":
+                self.currentStation?.Gust = self.foundCharacters
+            case "visibility":
+                self.currentStation?.Visibility = self.foundCharacters
+            case "weather":
+                self.currentStation?.Weather = self.foundCharacters
+            case "pressure":
+                self.currentStation?.Pressure = Int(self.foundCharacters)
+            case "tendency":
+                self.currentStation?.Tendency = self.foundCharacters
+            default:
+                print(self.foundCharacters)
+        }
+        
+        self.foundCharacters = ""
     }
-    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
